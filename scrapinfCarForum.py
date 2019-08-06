08075cdf32a98870bba70d8d829a2e304b9409e5
Fabián Ardila <fardila@Fabians-MacBook-Pro.local>
2019-08-06 18:19:18 -0500

    Organize folder to production

--- scrapinfCarForum.py
+++
@@ -1,6 +0,0 @@
-#Importing packages
-from selenium import webdriver
-
-driver = webdriver.Chrome("C:\\Users\\Fabian Ardila\\Desktop\\chromedriver.exe")
-
-driver.get('https://forums.edmunds.com/discussion/2864/general/x/entry-level-luxury-performance-sedans/p702')
