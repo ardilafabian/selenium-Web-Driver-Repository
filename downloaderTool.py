77a3a18e79faff34f3d6b4bed0dedc74a5d4fdb4
Fabián Ardila <fardila@Fabians-MacBook-Pro.local>
2019-08-08 11:15:28 -0500

    Change path of chrome driver

--- downloaderTool.py
+++ downloaderTool.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pandas import DataFrame
 
-browser = webdriver.Chrome("./chromeDriverExec/ver76-0-3809-68/chromedriver")
+browser = webdriver.Chrome("./chromeDriverExec/ver75-0-3770-140/chromedriver")
 browser.implicitly_wait(15)
 
 def getItemsUrl(numPages, url_provided):
